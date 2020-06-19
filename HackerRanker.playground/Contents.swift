7119de2e3661611e8f9e33b1a06f59ff2301dbb4
Jerry Ren <yren@gm.slc.edu>
2020-06-19 19:17:43 -0400

    playgroundPlayaIntegration

--- HackerRanker.playground/Contents.swift
+++ HackerRanker.playground/Contents.swift
@@ -1460,3 +1460,43 @@ import UIKit
 //
 //print(cutTheSticks(arr: [8, 8, 14, 10, 3, 5, 14, 12]))
 //print(cutTheSticks(arr: [1, 1, 1, 1,1,1,1,1,1,1,1,1,1,1,1,1,1]))
+
+
+// MARK: - Binary & Linear search
+
+//let numbersForTestings = [1, 2, 4, 6, 8, 9, 11, 13, 16, 17, 20]
+//
+//func linearSearch(searchValue: Int, array: [Int]) -> Bool {
+//    for num in array {
+//        if num == searchValue {
+//            return true
+//        }
+//    }
+//    return false
+//}
+//print(linearSearch(searchValue: 11, array: numbersForTestings))
+//
+//func binarySearch(searchValue: Int, array: [Int]) -> Bool {
+//
+//    var rightIndex = array.count - 1
+//    var leftIndex = 0
+//
+//    while leftIndex <= rightIndex {
+//
+//        let middleIndex = Int((leftIndex + rightIndex) / 2)
+//        let middleValue = array[middleIndex]
+//
+//        if middleValue == searchValue {
+//            return true
+//        }
+//        if searchValue < middleValue {
+//            rightIndex = middleIndex - 1
+//        }
+//        if searchValue > middleValue {
+//            leftIndex = middleIndex + 1
+//        }
+//    }
+//    return false
+//}
+//print(binarySearch(searchValue: 18, array: numbersForTestings))
+
