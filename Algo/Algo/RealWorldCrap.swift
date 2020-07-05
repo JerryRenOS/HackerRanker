2b314396c86828557e4cc73a9167d6b76b375feb
Jerry Ren <yren@gm.slc.edu>
2020-07-04 23:58:27 -0400

    RworldCrap

---
+++ Algo/Algo/RealWorldCrap.swift
@@ -0,0 +1,146 @@
+//
+//  RealWorldCrap.swift
+//  Algo
+//
+//  Created by Jerry Ren on 7/4/20.
+//  Copyright © 2020 Jerry Ren. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+class FriendsTVSeries {
+    
+    var episodeNumber: Int
+    var seasonNumber: Int
+    
+    init(episodeNumber: Int, seasonNumber:Int) {
+        self.episodeNumber = episodeNumber
+        self.seasonNumber = seasonNumber
+    }
+    
+    func hangOut(people: [MainCharactersName]) {
+        var description: String = ""
+        for individual in people {
+           description += "\(individual) and"
+        }
+        description += "are hanging out"
+        print(description)
+    }
+    
+    func getMarried(a: FriendsMainCharacter, b: FriendsMainCharacter) {
+        print("\(a) and \(b) are getting married")
+    }
+
+    
+    enum SignificantLocations {
+        case CentralPerkCoffeHouse
+        case MonicaAndRachelsApartment
+        case ChandlerAndJoeysApartment
+    }
+}
+
+struct FriendsMainCharacter {
+    
+    var name: MainCharactersName
+    var age: Int
+    var personality: PersonalityType
+    var spouse: String?
+    
+//    init() {}
+}
+
+enum MainCharactersName {
+    case Joey
+    case Chandler
+    case Phoebe
+    case Rachel
+    case Ross
+    case Monica
+}
+
+enum PersonalityType {
+    case INFP
+    case INTP
+    case INFJ
+    case INTJ
+    case ENTJ
+    case ENFJ
+    case ENFP
+    case ENTP
+    case ESTP
+    case ESFP
+    case ESFJ
+    case EFTJ
+    case ISTJ
+    case ISFJ
+    case ISFP
+    case ISTP
+}
+
+struct SupportingCharacters {
+    var characterName: String
+    init(characterName: String) {
+        self.characterName = characterName
+    }
+}
+
+
+let friend = FriendsTVSeries.init(episodeNumber: 11, seasonNumber: 6)
+// friend.hangOut(people: [.Chandler, .Phoebe, .Rachel])
+
+      
+
+        
+// MARK: - examples of enum's helpfulness
+
+enum CompassPoint {
+    case north, south, east, west
+    mutating func turnNorth() {
+        self = .north
+    }
+}
+var currentDirection = CompassPoint.west
+let rememberedDirection = currentDirection
+// currentDirection.turnNorth()
+
+
+
+// MARK: - NYC (perhaps dead-end)
+
+class NewYorkCity {
+    
+    var districts: Boroughs = Boroughs.init()
+    
+    var nicknames: Array<String>
+        = ["The Big Apple", "The City That Never Sleeps", "Empire City", "Gotham", "The Melting Pot"]
+    
+    var bridges: [String] = GloballyUsed.nycBridges
+             
+    // what kinda behaviors / functions can I write for NYC?  Darn it! It's not an animal or a character
+    
+}
+                  
+struct Boroughs {
+    let Manhattan: String = "Manhattan"
+    let Brooklyn: String = "Brooklyn"
+    let Bronx: String = "Bronx"
+    let Queens: String = "Queens"
+    let StatenIsland: String = "Staten Island"
+}
+
+struct SightSeeing {
+    let traditionalSpots = ["Central Park", "World Trade Center", "Statue of Liberty", "Metropolitan Museum", "Times Square", "Rockefeller Center", "New York Public Library", "Grand Central Terminal"]
+    
+    let lessKnownSpots = ["MOMA", "East Village", "Madison Square Garden","Coney Island Amusement Park", "Friends' Apartment", "Roosevelt Island Aerial Tram", "Rockaway Beach", "Hudson Yards", "Bronx Zoo", "Governor's Island"]
+}
+         
+struct GloballyUsed {
+    
+    static let nycBridges: [String] = ["Williamsburg Bridge", "George Washington Bridge","Manhattan Bridge", "Henry Hudson Bridge", "Queensboro Bridge", "Bronx-Whitestone Bridge", "Brooklyn Bridge"]
+    
+}
+ 
+
