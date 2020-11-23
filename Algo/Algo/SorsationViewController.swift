12dce438a68ee3d63d95ff35aa981d4986651014
Jerry Ren <jerryren.ios@gmail.com>
2020-11-22 23:57:45 -0500

    (unsorted: [T], by comparison: (T, T) -> Bool) -> [T]

--- Algo/Algo/SorsationViewController.swift
+++ Algo/Algo/SorsationViewController.swift
@@ -33,6 +33,23 @@ private func genericInsertionality<T: Comparable>(unsorted: [T], by comparison: 
     return Array<T>.init()
 }
 
+//func insertionSort<T: Comparable>(_ input: [T], by comparison: (T, T) -> Bool) -> [T]
+//{
+//    var items = input
+//
+//    for index in 1..<items.count
+//    {
+//        let value = items[index]
+//        var position = index
+//
+//        while position > 0 && comparison(items[position - 1], value) {
+//            items[position] = items[position - 1]
+//            position -= 1
+//        }
+//        items[position] = value
+//    }
+//    return items
+//}
 
 
 func insertionality(unsorted: inout[Int]) -> [Int] {
