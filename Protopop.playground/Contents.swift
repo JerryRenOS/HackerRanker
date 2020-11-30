361cf819aa8f34d5e77f9df0b7aa3733292922f2
Jerry Ren <jerryren.ios@gmail.com>
2020-11-29 23:58:24 -0500

    fileprivate func dmy()

--- Protopop.playground/Contents.swift
+++ Protopop.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Foundation
 import UIKit
 
-// 9.24 & 9.30 //
+// 9.24 & 9.30 & 10.8
 
 
 // 9/24 below ____________________________
@@ -107,12 +107,25 @@ extension Logs {
 }
 
 // 1. Default protocol implementation
-
 // 2. Creating optional and required method for protocol
 
-// 3. What type of property i can create inside protocol and its extension
-
-// 4. Does init method allowed inside protocol
-
-
 typealias CodableNewProtocolAlias = Encodable & Decodable
+
+
+// October 8th
+// tuples vs array
+fileprivate func dmy() -> (date: Int, month: String, year: Int) {
+    return (2020, "December", 17)
+}
+
+let d = dmy()
+var date = d.date
+date += 1
+print(date)
+
+fileprivate func dmy2() -> [Any] {
+    return ([2020, "December", 17] as? [Any])!
+}
+
+let date2 = dmy2()[0] as! Int
+print(date2)
